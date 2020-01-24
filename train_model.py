a3d7ec10f8b8fa221073806bd5ac4632312301c2
xiebin95 <33597708+xiebin95@users.noreply.github.com>
2020-01-24 21:11:10 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
 #train
 import os
 # 使用第二张GPU卡
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 # import os
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
